316192f6b04b7ff6299e2183e846638f5b05ba16
anas ismail <msanasismail@gmail.com>
2024-07-29 12:40:42 +0530

    top res up

--- src/components/TopBrands.jsx
+++ src/components/TopBrands.jsx
@@ -7,24 +7,42 @@ import Puma from "/assets/images/puma.jpg"
 
 const TopBrands = () => {
   return (
-    <div className='bg-[#3C4242] w-[90%] flex flex-col items-center justify-center gap-8 p-11 rounded-lg mt-12'>
-      <h1 className='text-4xl font-extrabold text-white'>Top Brands Deals</h1>
-      <p className='text-white font-extralight'>Up To <span className='text-[#FBD103]'>60%</span> off on brands</p>
-      <ul className='w-[80%] flex items-center justify-center gap-4'>
+    <div className='bg-[#3C4242] w-[90%] flex flex-col items-center justify-center gap-8 rounded-lg mt-12
+    xsm:p-6
+    md:p-11'>
+      <h1 className='text-4xl font-extrabold text-white
+      xsm:text-2xl
+      md:text-4xl'>Top Brands Deals</h1>
+      <p className='text-white font-extralight
+      xsm:text-sm
+      md:text-lg'>Up To <span className='text-[#FBD103]'>60%</span> off on brands</p>
+      <ul className='w-[80%] flex items-center justify-center
+      xsm:gap-2 xsm:w-[95%]
+      md:gap-4 md:w-[80%]'>
         <li className=''>
-            <img className='rounded-lg' src={Nike} alt="nike" />
+            <img className='
+            xsm:rounded-sm
+            md:rounded-lg' src={Nike} alt="nike" />
         </li>
         <li className=''>
-            <img className='rounded-lg' src={HandM} alt="h&m" />
+            <img className='
+             xsm:rounded-sm
+             md:rounded-lg' src={HandM} alt="h&m" />
         </li>
         <li className=''>
-            <img className='rounded-lg' src={Levis} alt="levis" />
+            <img className='
+             xsm:rounded-sm
+             md:rounded-lg' src={Levis} alt="levis" />
         </li>
         <li className=''>
-            <img className='rounded-lg' src={USPA} alt="uspa" />
+            <img className='
+             xsm:rounded-sm
+             md:rounded-lg' src={USPA} alt="uspa" />
         </li>
         <li className=''>
-            <img className='rounded-lg' src={Puma} alt="puma" />
+            <img className='
+             xsm:rounded-sm
+             md:rounded-lg' src={Puma} alt="puma" />
         </li>
        
       </ul>
